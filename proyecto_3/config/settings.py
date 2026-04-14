07217a6943764bc6fe54003d9eeac0a6bdea2104
jorgeluismojica@soysena.edu.co <jorge_mojica@soy.sena.edu.co>
2026-04-14 10:18:51 -0500

    r

--- proyecto_3/config/settings.py
+++ proyecto_3/config/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME', 'proyecto'),
         'USER': os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'aprendermysql123'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'parceroj'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '3306'),
         'OPTIONS': {
